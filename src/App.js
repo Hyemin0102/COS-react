1f54176c89c41dae7725642a65935f2007be0f57
Hyemin0102 <128768462+Hyemin0102@users.noreply.github.com>
2023-09-03 05:37:12 +0900

    outlet 수정

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import { AuthContextPorvider } from "./components/context/AuthContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+
 const queryClient = new QueryClient();
 
 //3.context 사용할 곳 상위 파일에서 감싸줌(App.js or index.js 상황에 맞게)
